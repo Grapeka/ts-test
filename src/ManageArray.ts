06a323a648640b83898498c10876a43bf5fd2073
Grapeka <chotwilaiwan@gmail.com>
2022-12-02 16:13:53 +0700

    feat: add sort method

--- src/ManageArray.ts
+++ src/ManageArray.ts
@@ -1,7 +1,22 @@
-import { IArray } from "./merge";
+import { IArray } from "./IArray";
 
 export class ManageArray implements IArray {
+  sort(collection: number[]): number[] {
+    let temp,
+      arr = collection;
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = i + 1; j < arr.length; j++) {
+        if (arr[i] > arr[j]) {
+          temp = arr[i];
+          arr[i] = arr[j];
+          arr[j] = temp;
+        }
+      }
+    }
+    return arr;
+  }
+
   merge(collection_1: number[], collection_2: number[]): number[] {
-    return collection_1.concat(collection_2);
+    return this.sort(collection_1.concat(collection_2));
   }
 }
