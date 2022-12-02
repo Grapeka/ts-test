50306f995290bcf1cbfda7cbbbf6bfd07d5c2856
Grapeka <chotwilaiwan@gmail.com>
2022-12-02 16:14:12 +0700

    feat: update test script

--- __test__/merge.test.ts
+++ __test__/merge.test.ts
@@ -3,7 +3,7 @@ import { ManageArray } from "../src/ManageArray";
 describe("Merge", () => {
   it("should merge two arrays", () => {
     const manageArray = new ManageArray();
-    const result = manageArray.merge([1, 2, 3], [4, 5, 6]);
+    const result = manageArray.merge([1, 2, 3], [5, 4, 6]);
     expect(result).toEqual([1, 2, 3, 4, 5, 6]);
   });
 });
